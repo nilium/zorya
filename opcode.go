59ece973a68281a91a5652332f88a8621fa24526
Noel Cower <ncower@gmail.com>
2015-01-18 12:05:24 -0800

    Reserve bits of Opcode for attributes.
    Currently, the only attributes are opDest and the operand count for an
    opcode.
    
    Also add a few convenience methods: strict, valid, attrs, and hasDst.

--- opcode.go
+++ opcode.go
@@ -1,126 +1,167 @@
 package zorya
 
-// Opcode represents a single integer opcode in Zorya.
+// Opcode represents a single integer opcode in Zorya. The lower 16 bits are
+// reserved for the Opcode integer itself (a 16-bit integer) and the remaining
+// upper 15 bits, excluding sign bit, are reserved for attribute flags set on
+// Opcodes to determine their usage (e.g., opDest signals that the first
+// operand is a destination register).
+//
+// Attributes are not stored in bytecode.
 type Opcode int
 
+// Opcode attributes
 const (
-	opAdd Opcode = iota
-	opSub
-	opDiv
-	opIDiv
-	opMul
-	opPow
-	opMod
-	opIMod
-	opNeg
-	opNot
-	opOr
-	opAnd
-	opXor
-	opArithShift
-	opBitShift
-	opFloor
-	opCeil
-	opRound
-	opRInt
-	opEq
-	opLE
-	opLT
-	opJump
-	opPush
-	opPop
-	opLoad
-	opCall
-	opReturn
-	opRealloc
-	opFree
-	opMemmove
-	opTrap
-	opMemdup
-	opMemlen
-	opPeek
-	opPoke
-	opDefer
-	opForce
+	// opDest marks an Opcode as having an output as its first operand.
+	opDest = 1<<18 + iota
 )
+
+const (
+	opArgShift = 28
+	opArgMask  = 0x7
+)
+
+// These would be better as const structs if that were possible, but for now,
+// this works.
+const (
+	// NAME             = iota | (ARGC)            | ATTRS
+	OpAdd        Opcode = iota | (3 << opArgShift) | opDest
+	OpSub        Opcode = iota | (3 << opArgShift) | opDest
+	OpDiv        Opcode = iota | (3 << opArgShift) | opDest
+	OpIDiv       Opcode = iota | (3 << opArgShift) | opDest
+	OpMul        Opcode = iota | (3 << opArgShift) | opDest
+	OpPow        Opcode = iota | (3 << opArgShift) | opDest
+	OpMod        Opcode = iota | (3 << opArgShift) | opDest
+	OpIMod       Opcode = iota | (3 << opArgShift) | opDest
+	OpArithShift Opcode = iota | (3 << opArgShift) | opDest
+	OpBitShift   Opcode = iota | (3 << opArgShift) | opDest
+	OpOr         Opcode = iota | (3 << opArgShift) | opDest
+	OpAnd        Opcode = iota | (3 << opArgShift) | opDest
+	OpXor        Opcode = iota | (3 << opArgShift) | opDest
+	OpNot        Opcode = iota | (2 << opArgShift) | opDest
+	OpNeg        Opcode = iota | (2 << opArgShift) | opDest
+	OpFloor      Opcode = iota | (2 << opArgShift) | opDest
+	OpCeil       Opcode = iota | (2 << opArgShift) | opDest
+	OpRound      Opcode = iota | (2 << opArgShift) | opDest
+	OpRInt       Opcode = iota | (2 << opArgShift) | opDest
+	OpEq         Opcode = iota | (3 << opArgShift)
+	OpLE         Opcode = iota | (3 << opArgShift)
+	OpLT         Opcode = iota | (3 << opArgShift)
+	OpJump       Opcode = iota | (1 << opArgShift)
+	OpPush       Opcode = iota | (1 << opArgShift)
+	OpPop        Opcode = iota | (1 << opArgShift) | opDest
+	OpLoad       Opcode = iota | (2 << opArgShift) | opDest
+	OpCall       Opcode = iota | (2 << opArgShift)
+	OpReturn     Opcode = iota | (0 << opArgShift) | opDest
+	OpRealloc    Opcode = iota | (3 << opArgShift) | opDest
+	OpFree       Opcode = iota | (1 << opArgShift)
+	OpMemmove    Opcode = iota | (5 << opArgShift) | opDest
+	OpTrap       Opcode = iota | (0 << opArgShift) | opDest
+	OpMemdup     Opcode = iota | (2 << opArgShift) | opDest
+	OpMemlen     Opcode = iota | (2 << opArgShift) | opDest
+	OpPeek       Opcode = iota | (4 << opArgShift) | opDest
+	OpPoke       Opcode = iota | (4 << opArgShift)
+	OpDefer      Opcode = iota | (1 << opArgShift) | opDest
+	OpForce      Opcode = iota | (2 << opArgShift) | opDest
+)
+
+func (op Opcode) argc() int {
+	return int(op>>opArgShift) & opArgMask
+}
+
+func (op Opcode) valid() bool {
+	op = op.strict()
+	return op >= OpAdd.strict() && op <= OpForce.strict()
+}
+
+func (op Opcode) strict() Opcode {
+	return op & 0xFFFF
+}
+
+func (op Opcode) attrs() int {
+	return int(op) & 0x7FFF0000
+}
+
+func (op Opcode) hasDst() bool {
+	return op&opDest == opDest
+}
 
 func (op *Opcode) String() string {
 	switch *op {
-	case opAdd:
+	case OpAdd:
 		return "Add"
-	case opSub:
+	case OpSub:
 		return "Sub"
-	case opDiv:
+	case OpDiv:
 		return "Div"
-	case opIDiv:
+	case OpIDiv:
 		return "IDiv"
-	case opMul:
+	case OpMul:
 		return "Mul"
-	case opPow:
+	case OpPow:
 		return "Pow"
-	case opMod:
+	case OpMod:
 		return "Mod"
-	case opIMod:
+	case OpIMod:
 		return "IMod"
-	case opNeg:
+	case OpNeg:
 		return "Neg"
-	case opNot:
+	case OpNot:
 		return "Not"
-	case opOr:
+	case OpOr:
 		return "Or"
-	case opAnd:
+	case OpAnd:
 		return "And"
-	case opXor:
+	case OpXor:
 		return "Xor"
-	case opArithShift:
+	case OpArithShift:
 		return "ArithShift"
-	case opBitShift:
+	case OpBitShift:
 		return "BitShift"
-	case opFloor:
+	case OpFloor:
 		return "Floor"
-	case opCeil:
+	case OpCeil:
 		return "Ceil"
-	case opRound:
+	case OpRound:
 		return "Round"
-	case opRInt:
+	case OpRInt:
 		return "RInt"
-	case opEq:
+	case OpEq:
 		return "Eq"
-	case opLE:
+	case OpLE:
 		return "LE"
-	case opLT:
+	case OpLT:
 		return "LT"
-	case opJump:
+	case OpJump:
 		return "Jump"
-	case opPush:
+	case OpPush:
 		return "Push"
-	case opPop:
-		return "Pop"
-	case opLoad:
+	case OpPop:
+		return "POp"
+	case OpLoad:
 		return "Load"
-	case opCall:
+	case OpCall:
 		return "Call"
-	case opReturn:
+	case OpReturn:
 		return "Return"
-	case opRealloc:
+	case OpRealloc:
 		return "Realloc"
-	case opFree:
+	case OpFree:
 		return "Free"
-	case opMemmove:
+	case OpMemmove:
 		return "Memmove"
-	case opTrap:
+	case OpTrap:
 		return "Trap"
-	case opMemdup:
+	case OpMemdup:
 		return "Memdup"
-	case opMemlen:
+	case OpMemlen:
 		return "Memlen"
-	case opPeek:
+	case OpPeek:
 		return "Peek"
-	case opPoke:
+	case OpPoke:
 		return "Poke"
-	case opDefer:
+	case OpDefer:
 		return "Defer"
-	case opForce:
+	case OpForce:
 		return "Force"
 	default:
 		return "<UNKNOWN>"
